fcc61db5c8e9230beaafb2fa8226afda9d2dfd68
Jan Chren <dev.rindeal@gmail.com>
2021-10-13 04:48:23 +0000

    github CI: add windows builds

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 @echo off
 
-cl *.cpp /EHsc /nologo /Fe:amalgamate /link ole32.lib
+cl ^
+  /EHsc /nologo ^
+  /I3rd\JUCE\modules\juce_core /I.\ ^
+  /D"JUCE_APP_CONFIG_HEADER=""AppConfig.h""" ^
+  Amalgamate.cpp 3rd\JUCE\modules\juce_core\juce_core.cpp ^
+  /Fe:amalgamate ^
+  /link ole32.lib Shell32.lib
+
 del *.obj
