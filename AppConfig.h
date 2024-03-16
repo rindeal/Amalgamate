d24d4e84084f5bebdb3d92a4b74a5cd32be7b163
rindeal <dev.rindeal@gmail.com>
2024-03-16 20:14:00 +0000

    AppConfig.h: refactor

--- AppConfig.h
+++ AppConfig.h
@@ -1,21 +1,24 @@
 // Configures the Juce library.
 
-#ifndef   JUCE_FORCE_DEBUG
-//#define JUCE_FORCE_DEBUG
-#endif
-
-#ifndef   JUCE_LOG_ASSERTIONS
-//#define JUCE_LOG_ASSERTIONS
-#endif
-
-#ifndef   JUCE_CHECK_MEMORY_LEAKS
-//#define JUCE_CHECK_MEMORY_LEAKS
-#endif
-
 #ifndef   JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES
 //#define JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES
 #endif
 
-//#define JUCE_INCLUDE_ZLIB_CODE 1
 #define JUCE_USE_CURL 0
 #define JUCE_STANDALONE_APPLICATION 1
+
+#ifdef JUCE_DEBUG
+#
+#  ifndef   JUCE_FORCE_DEBUG
+#    define JUCE_FORCE_DEBUG 1
+#  endif
+#
+#  ifndef   JUCE_LOG_ASSERTIONS
+#    define JUCE_LOG_ASSERTIONS 1
+#  endif
+#
+#  ifndef   JUCE_CHECK_MEMORY_LEAKS
+#    define JUCE_CHECK_MEMORY_LEAKS 1
+#  endif
+#
+#endif
